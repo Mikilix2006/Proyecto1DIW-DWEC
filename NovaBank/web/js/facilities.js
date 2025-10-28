a19c24142103172c6bb342bd0aa826ee8368d95b
Habib Garcia <hab.garcia07@gmail.com>
2025-10-28 12:31:26 +0100

    Implementacion de la css alternativa v01

--- NovaBank/web/js/facilities.js
+++ NovaBank/web/js/facilities.js
@@ -11,4 +11,30 @@ window.addEventListener('click', (e) => {
     }
 });
 
+//css alternativa
+const themeBtn = document.getElementById('themeToggle');
+const themeLink = document.getElementById('theme-link');
+const body = document.body;
+
+// Verifica si el usuario ya tiene un tema guardado
+const currentTheme = localStorage.getItem('theme') || 'light';
+if (currentTheme === 'dark') {
+    themeLink.setAttribute('href', 'oscuro.css');
+    body.classList.add('dark-mode');
+  }
+
+  // Alterna el tema
+  themeBtn.addEventListener('click', () => {
+    const isDark = themeLink.getAttribute('href') === 'oscuro.css';
+    if (isDark) {
+      themeLink.setAttribute('href', 'style.css');
+      body.classList.remove('dark-mode');
+      localStorage.setItem('theme', 'light');
+    } else {
+      themeLink.setAttribute('href', 'oscuro.css');
+      body.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark');
+    }
+  });
+
 //ojito de contraseña
