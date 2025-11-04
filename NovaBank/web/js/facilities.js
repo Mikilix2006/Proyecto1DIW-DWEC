9fb852da3f93d339532b02dbc67aeb595f7c24a9
Habib Garcia <hab.garcia07@gmail.com>
2025-11-04 11:47:38 +0100

    Actualización

--- NovaBank/web/js/facilities.js
+++ NovaBank/web/js/facilities.js
@@ -38,3 +38,33 @@ window.addEventListener('click', (e) => {
     }
 });
 //ojito de contraseña
+document.addEventListener("Click", () => {
+  const togglePassword = document.getElementById("togglePassword");
+  const passwordInput = document.getElementById("tfPassword");
+  const eyeIcon = document.getElementById("eyeIcon");
+
+  if (togglePassword && passwordInput && eyeIcon) {
+    togglePassword.addEventListener("click", () => {
+
+      // Si la contraseña está oculta, la mostramos
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+
+        // Cambiamos el icono a ojo cerrado
+        eyeIcon.innerHTML = `
+          <path d="M1 1l22 22M12 5c-7.633 0-11 7-11 7s3.367 7 11 7c1.816 0 3.486-.342 5.01-.96M15 15a3 3 0 0 1-4.24-4.24M9 9a3 3 0 0 1 4.24 4.24"/>
+        `;
+      } 
+      // Si está visible, la volvemos a ocultar
+      else {
+        passwordInput.type = "password";
+
+        // Cambiamos el icono a ojo abierto
+        eyeIcon.innerHTML = `
+          <path d="M12 5c-7.633 0-11 7-11 7s3.367 7 11 7 11-7 11-7-3.367-7-11-7zm0 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10z"/>
+        `;
+      }
+
+    });
+  }
+});
