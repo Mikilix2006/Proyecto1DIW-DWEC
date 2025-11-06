a9b95962651f7df909b7d2ced5f6e861426fc881
Tu sayuri <sayuri@gmail.com>
2025-11-06 13:38:17 +0100

    Pruebas

--- NovaBank/web/js/changePassword.js
+++ NovaBank/web/js/changePassword.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//Prueba
 // === VALIDAR CONTRASEÑA ACTUAL ===
 function validar_PasswordActual() {
     const passwordActual = document.getElementById("password");
