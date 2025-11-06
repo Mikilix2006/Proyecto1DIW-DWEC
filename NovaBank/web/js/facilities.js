a8dbdab72cbaea22547e93c6cffeb98ceb75224f
Habib Garcia <hab.garcia07@gmail.com>
2025-11-06 09:50:44 +0100

    Borrar historial de datos al salir de la sesión

--- NovaBank/web/js/facilities.js
+++ NovaBank/web/js/facilities.js
@@ -12,3 +12,24 @@ window.addEventListener('click', (e) => {
 });
 
 //ojito de contraseña
+
+//CERRR SESSION
+function salirSession(){
+    document.getElementById('botonCerrarSesion').addEventListener('click', function() {
+    event.preventDefault();
+    event.stopPropagation();
+    sessionStorage.removeItem('customer.id');
+    sessionStorage.removeItem('customer.firstName');
+    sessionStorage.removeItem('customer.lastName');
+    sessionStorage.removeItem('customer.middleInitial');
+    sessionStorage.removeItem('customer.street');
+    sessionStorage.removeItem('customer.city');
+    sessionStorage.removeItem('customer.state');
+    sessionStorage.removeItem('customer.zip');
+    sessionStorage.removeItem('customer.phone');
+    sessionStorage.removeItem('customer.email');
+    sessionStorage.removeItem('customer.password');
+
+    window.location.href = 'index.html'; 
+    });
+}
