8bb722dfb8968beb1a29f3b78a5a4a1f34676ac8
Miguel González <miguel.gonzalezdelgado06@gmail.com>
2025-12-05 10:33:09 +0100

    Created accountsController.js and added info of the structure of the file

---
+++ NovaBank/web/js/accountsController.js
@@ -0,0 +1,87 @@
+/* 
+ * ================================================
+ * 
+ *  THIS JS FILE IS IN CHARGE OF THE BEHAVIOUR OF
+ *  THE ACCOUNTS SECTION LOCATED IN accounts.html
+ * 
+ * -----------------------------------------------
+ * 
+ * The file includes the recovery of the accounts
+ * that is going to be setted on a Map.
+ * 
+ * Then the accounts of a given Customer ID passed
+ * by the sessionStorage, will be displayed on a
+ * modificable table on accounts.html
+ * 
+ * The user can add, delete or modify data from
+ * that table and it will be checked in this file
+ * and posted, putted or deleted on the database.
+ * 
+ * AUTHOR: Miguel González Delgado
+ * CREATION DATE: 2025-12-5 9:08 UTC+1 
+ * 
+ * ================================================
+ */
+
+// possible feature
+class AccountController {
+    //attributes
+    //constructor
+}
+
+/*
+ * =================================================
+ * 
+ *     ATTRIBUTES TO BE USED BY THIS CONTROLLER
+ *     
+ * -------------------------------------------------
+ * 
+ * Takes the necessary elements from accounts.html
+ * to be handled later by the event handlers.
+ * 
+ * All the elements on the accounts.html that need
+ * to be handled, have an id called like it's name.
+ * 
+ * Each of those elements are going to have a
+ * reference with the element content taken by
+ * the method .getElementById()
+ * 
+ * =================================================
+ */
+
+/*
+ * =================================================
+ * 
+ *       LISTENERS FOR HANDLING EVENTS ON HTML
+ *       
+ * -------------------------------------------------
+ * 
+ * Listeners below triggers the handlers for the
+ * attributes taken from the form in accounts.html
+ * 
+ * =================================================
+ */
+
+/*
+ * =================================================
+ * 
+ *     ENEVT HANDLERS CALLED FROM THE LISTENERS
+ * 
+ * -------------------------------------------------
+ *     
+ * Functions below 
+ * 
+ * =================================================
+ */
+
+/*
+ * =================================================
+ * 
+ *                 OTHER FUNCTIONS
+ * 
+ * -------------------------------------------------
+ *     
+ * Functions below 
+ * 
+ * =================================================
+ */
