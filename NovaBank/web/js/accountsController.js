f185e3e71f940033369afc8ad846c5e340c75a43
Miguel González <miguel.gonzalezdelgado06@gmail.com>
2025-12-11 13:13:09 +0100

    Added more info of the structure of the file and methods

--- NovaBank/web/js/accountsController.js
+++ NovaBank/web/js/accountsController.js
@@ -1,87 +1,143 @@
 /* 
- * ================================================
- * 
- *  THIS JS FILE IS IN CHARGE OF THE BEHAVIOUR OF
- *  THE ACCOUNTS SECTION LOCATED IN accounts.html
- * 
- * -----------------------------------------------
- * 
- * The file includes the recovery of the accounts
- * that is going to be setted on a Map.
- * 
- * Then the accounts of a given Customer ID passed
- * by the sessionStorage, will be displayed on a
- * modificable table on accounts.html
- * 
- * The user can add, delete or modify data from
- * that table and it will be checked in this file
- * and posted, putted or deleted on the database.
- * 
- * AUTHOR: Miguel González Delgado
- * CREATION DATE: 2025-12-5 9:08 UTC+1 
- * 
- * ================================================
+   ================================================
+   
+    THIS JS FILE IS IN CHARGE OF THE BEHAVIOUR OF
+    THE ACCOUNTS SECTION LOCATED IN accounts.html
+   
+   -----------------------------------------------
+   
+   The file includes the recovery of the accounts
+   that is going to be setted on a Map.
+   
+   Then the accounts of a given Customer ID passed
+   by the sessionStorage, will be displayed on a
+   modificable table on html/accounts.html.
+   
+   The user can add, delete or modify data from
+   that table and it will be checked in this file
+   and posted, putted or deleted on the database.
+   
+   <·> <·> <·> <·> <·> <·> <·> <·> <·> <·> <·> <·> 
+   
+   AUTHOR: Miguel González Delgado
+   CREATION DATE: 2025-12-5 9:08 UTC+01
+   
+   ================================================
  */
 
-// possible feature
+//possible fearture
 class AccountController {
-    //attributes
     //constructor
+    constructor (id, description, balance, creditLine, beginBalance, beginBalanceTimestamp, type) {
+        this.id = id;
+        this.description = description;
+        this.balance = balance;
+        this.creditLine = creditLine;
+        this.beginBalance = beginBalance;
+        this.beginBalanceTimestamp = beginBalanceTimestamp;
+        this.type = type;
+    }
+    //getters
+    get id() {
+        return this.id;
+    }
+    //methods
+    getAccounts() {
+        
+    }
+    createAccount() {
+        
+    }
+    updateAccount(updatedAccount) {
+        
+    }
+    deleteAccount() {
+        
+    }
 }
 
 /*
- * =================================================
- * 
- *     ATTRIBUTES TO BE USED BY THIS CONTROLLER
- *     
- * -------------------------------------------------
- * 
- * Takes the necessary elements from accounts.html
- * to be handled later by the event handlers.
- * 
- * All the elements on the accounts.html that need
- * to be handled, have an id called like it's name.
- * 
- * Each of those elements are going to have a
- * reference with the element content taken by
- * the method .getElementById()
- * 
- * =================================================
+   =================================================
+   
+       ATTRIBUTES TO BE USED BY THIS CONTROLLER
+       
+   -------------------------------------------------
+   
+   Takes the necessary elements from
+   html/accounts.html to be handled later by the
+   event handlers.
+   
+   All the elements on the html/accounts.html that 
+   need to be handled, have an id called like 
+   it's name.
+   
+   Each of those elements are going to have an
+   instance with the element content taken by
+   the method reference.getElementById().
+   
+   Also a Map() will be instanced to contain
+   all the Accounts of the given Costumer ID.
+
+   The Map() structure is:
+   -> key = account.id
+   -> value = account (the object)
+
+   To add all the accounts to the Map() we will use
+   a for that runs over all the accounts recieved
+   by the fetch method and the GET request and
+   are going to be given in JSON format.
+   
+   =================================================
  */
 
-/*
- * =================================================
- * 
- *       LISTENERS FOR HANDLING EVENTS ON HTML
- *       
- * -------------------------------------------------
- * 
- * Listeners below triggers the handlers for the
- * attributes taken from the form in accounts.html
- * 
- * =================================================
- */
+const reference1 = document.getElementById('elementId');
+
+var accountsMap = new Map();
 
 /*
- * =================================================
- * 
- *     ENEVT HANDLERS CALLED FROM THE LISTENERS
- * 
- * -------------------------------------------------
- *     
- * Functions below 
- * 
- * =================================================
+   =================================================
+   
+         LISTENERS FOR HANDLING EVENTS ON HTML
+         
+   -------------------------------------------------
+   
+   These listeners triggers the handlers for the
+   attributes taken from the form in 
+   html/accounts.html when an event triggers caused 
+   by a modification on the form.
+   
+   =================================================
  */
 
+reference1.addEventListener('element',handleReferencia1OnEvent);
+
 /*
- * =================================================
- * 
- *                 OTHER FUNCTIONS
- * 
- * -------------------------------------------------
- *     
- * Functions below 
- * 
- * =================================================
+   =================================================
+   
+       ENEVT HANDLERS CALLED FROM THE LISTENERS
+   
+   -------------------------------------------------
+       
+   These functions are the triggered handlers from
+   the event listeners.
+
+   
+   
+   =================================================
+ */
+
+function handleReferencia1OnEvent() {
+    // handler
+}
+
+/*
+   =================================================
+   
+                   OTHER FUNCTIONS
+   
+   -------------------------------------------------
+       
+   These functions 
+   
+   =================================================
  */
