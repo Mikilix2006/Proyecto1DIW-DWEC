71acef667a202dae562a1afcbea65e1997cd8099
Habib Garcia <hab.garcia07@gmail.com>
2025-12-15 10:37:44 +0100

    Object model created

---
+++ NovaBank/web/js/model.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Movements {
+  constructor(id,timestamp,amount,balance,description) {
+    this.id=id;
+    this.timestamp=timestamp;
+    this.amount=amount;
+    this.balance=balance;
+    this.description=description;
+  }
+}
+
