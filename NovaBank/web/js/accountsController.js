05890180b63be47cecd4b69b56fa15a2eef60d33
Miguel González <miguel.gonzalezdelgado06@gmail.com>
2026-01-09 14:10:57 +0100

    Created new branch for accounts development

---
+++ NovaBank/web/js/accountsController.js
@@ -0,0 +1,176 @@
+/* 
+   ================================================
+   
+    THIS JS FILE IS IN CHARGE OF THE BEHAVIOUR OF
+    THE ACCOUNTS SECTION LOCATED IN cuentas.html
+   
+   -----------------------------------------------
+   
+   The file includes the recovery of the accounts
+   that is going to be setted on a Map.
+   
+   Then the accounts of a given Customer ID passed
+   by the sessionStorage, will be displayed on a
+   modificable table on html/accounts.html.
+   
+   The user can add, delete or modify data from
+   that table and it will be checked in this file
+   and posted, putted or deleted on the database.
+   
+   <·> <·> <·> <·> <·> <·> <·> <·> <·> <·> <·> <·> 
+   
+   AUTHOR: Miguel González Delgado
+   CREATION DATE: 2025-12-5 9:08 UTC+01
+   
+   ================================================
+ */
+
+/*
+   =================================================
+   
+       ATTRIBUTES TO BE USED BY THIS CONTROLLER
+       
+   -------------------------------------------------
+   
+   Takes the necessary elements from
+   html/accounts.html to be handled later by the
+   event handlers.
+   
+   All the elements on the html/accounts.html that 
+   need to be handled, have an id called like 
+   it's name.
+   
+   Each of those elements are going to have an
+   instance with the element content taken by
+   the method reference.getElementById().
+   
+   Also a Map() will be instanced to contain
+   all the Accounts of the given Costumer ID.
+
+   The Map() structure is:
+   -> key = account.id
+   -> value = AccountController
+
+   To add all the accounts to the Map() we will use
+   a for that runs over all the accounts recieved
+   by the fetch method and the GET request and
+   are going to be given in JSON format.
+   
+   =================================================
+ */
+
+//const id = document.getElementById('id');
+//const description = document.getElementById('description');
+//const balance = document.getElementById('balance');
+//const creditLine = document.getElementById('creditLine');
+//const beginBalance = document.getElementById('beginBalance');
+//const beginBalanceTimestamp = document.getElementById('beginBalanceTimestamp');
+//const type = document.getElementById('type');
+//
+//var accountsMap = new Map();
+//
+//var ac = new AccountController(id.value.trim(),
+//                               description.value.trim(),
+//                               balance.value.trim(),
+//                               creditLine.value.trim(),
+//                               beginBalance.value.trim(),
+//                               beginBalanceTimestamp.value.trim(),
+//                               type.value.trim());
+//
+//// Crear una cuenta siempre despues de una confirmación
+//accountsMap = ac.createAccount(ac);
+
+/*
+   =================================================
+   
+         LISTENERS FOR HANDLING EVENTS ON HTML
+         
+   -------------------------------------------------
+   
+   These listeners triggers the handlers for the
+   attributes taken from the form in 
+   html/accounts.html when an event triggers caused 
+   by a modification on the form.
+   
+   =================================================
+ */
+
+//reference1.addEventListener('element',handleReferencia1OnEvent);
+
+/*
+   =================================================
+   
+       ENEVT HANDLERS CALLED FROM THE LISTENERS
+   
+   -------------------------------------------------
+       
+   These functions are the triggered handlers from
+   the event listeners.
+
+   
+   
+   =================================================
+ */
+
+function handleReferencia1OnEvent() {
+    // handler
+}
+
+/*
+   =================================================
+   
+                   OTHER FUNCTIONS
+   
+   -------------------------------------------------
+       
+   These functions 
+   
+   =================================================
+ */
+
+function* generateAccountRow(accounts) {
+    // generator
+    
+}
+
+
+console.log("Hola 1");
+const SERVICE_URL = "/CRUDBankServerSide/webresources/account/customer/";
+const idCustomer = "299985563";
+const msgBoxAccounts = document.getElementById('msgBoxAccounts');
+fetch(SERVICE_URL +`${encodeURIComponent(idCustomer)}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                //body: JSON.stringify(account)   <===   ???
+            }).then(response => {
+                // PROCESADO DE RESPUESTA 500
+                if (response.status===500) {
+                    return response.text().then(text => {
+                        throw new Error("An error happend, try again and if the error persists, try again later");
+                    }); // Fin del return
+                } // Fin del if
+                // PROCESADO DE RESPUESTA DESCONOCIDA
+                // Ha habido un error inesperado
+                else if (!response.ok) {
+                    return response.text().then(text => {
+                        throw new Error(text || "An unknown error happend");
+                    }); // Fin del return
+                } // Fin del if
+                return response;
+            })
+            // PROCESAR RESPUESTA OK
+                .then(data => {
+                    msgBoxAccounts.style.color = "#5620ad";
+                    msgBoxAccounts.textContent = "Todo bien";
+                    msgBoxAccounts.style.display = 'block';
+                    return data;
+            })
+            // MOSTRAR ERRORES
+                .catch(e => {
+                    msgBoxAccounts.style.color = "#ff0000";
+                    msgBoxAccounts.textContent = "Error: " + e.message;
+                    msgBoxAccounts.style.display = 'block';
+            });
---
+++ NovaBank/web/js/model.js
@@ -0,0 +1,233 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+class AccountController {
+    //constructor
+    constructor (id, description, balance, creditLine, beginBalance, beginBalanceTimestamp, type) {
+        this.id = id;
+        this.description = description;
+        this.balance = balance;
+        this.creditLine = creditLine;
+        this.beginBalance = beginBalance;
+        this.beginBalanceTimestamp = beginBalanceTimestamp;
+        this.type = type;
+    }
+    //getters
+    get id() {
+        return this.id;
+    }
+    //methods
+    /*
+       =================================================
+
+            METHODS OF THE CLASS ACCOUNTCONTROLLER
+
+       -------------------------------------------------
+
+       Executes every CRUD action and throws exceptions
+       in case the method didn't succeed.
+       
+       Possible HTTP responses:
+       -> 500 = Server not avalible or the ID of the
+                new account already exists (POST)
+       -> 200 = The GET method returned data (json)
+       -> 204 = Everything went well (POST, PUT, DELETE)
+    
+       · METHOD: getAccountsByCustomerID(customerID)
+            ~ @param {number} customerID is the
+                ID from which all the accounts will
+                be taken.
+            ~ @returns {Map()} with a structure like:
+                -> key = account.id
+                -> value = AccountController
+            ~ @throws {Error} with the message if the 
+                server side returns a 500.
+            ~ Takes the customerID from the parameters
+                and searches for all accounts that
+                are related to that specific customerID
+                saving the accountID in the key of a
+                Map() and the actual account on the
+                value.
+                The method uses a GET request to
+                search for all the accounts.
+            ~ Usable by: Admin & User
+    
+       · METHOD: createAccount(account)
+            ~ @param {AccountController} account is 
+                the account that the user wants to be 
+                created in the server side (POST).
+            ~ @returns {Map()} returns the updated Map() 
+                if the account has successfuly been 
+                created in the server side.
+            ~ @throws {Error} with the message if the 
+                server side returns a 500.
+            ~ Takes the customer data from the
+                sessionStorage and the account passed
+                by parameter and creates a structure
+                in json to be sent by a POST.
+                It also calls the 
+                getAccountsByCustomerID method to 
+                update the Map() and return it.
+            ~ Usable by: Admin
+    
+       · METHOD: updateAccount(updatedAccount)
+            ~ @param {AccountController} updatedAccount 
+                is the new account that is going to
+                replace the old one.
+            ~ @returns {Map()} returns the updated Map() 
+                if the account has successfuly been 
+                updated in the server side.
+            ~ @throws {Error} with the message if the 
+                server side returns a 500.
+            ~ Takes the customer data from the
+                sessionStorage and the account passed
+                by parameter and creates a structure
+                in json to be sent by a POST.
+                It also calls the 
+                getAccountsByCustomerID method to update
+                the Map().
+            ~ Usable by: Admin & User
+    
+       · METHOD: deleteAccount(accountID)
+            ~ @param {number} accountID is the ID of 
+                the account that is going to be deleted.
+            ~ @returns {Map()} returns the updated Map() 
+                if the account has successfuly been 
+                deleted in the server side.
+            ~ @throws {Error} with the message if the 
+                server side returns a 500.
+            ~ Takes the account ID and checks if it is
+                empty (of movements) and if so, it deletes
+                the account, if it still has movements,
+                will advice the admin and tell him why he
+                couldn't delete the account.
+                It also calls the 
+                getAccountsByCustomerID method to update
+                the Map().
+            ~ Usable by: Admin
+
+       =================================================
+     */
+    getAccountsByCustomerID(customerID) {
+        // coger referencia de capa para mensaje 
+        // de informacion al usuario
+        const msgBoxAccounts = document.getElementById('msgBoxAccounts');
+        // Crear mapa para almacenar cuentas
+        var map = new Map();
+        
+        fetch("/CRUDBankServerSide/webresources/account",
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                //body: JSON.stringify(account)   <===   ???
+            }).then(response => {
+                // PROCESADO DE RESPUESTA 500
+                if (response.status===500) {
+                    return response.text().then(text => {
+                        throw new Error("An error happend, try again and if the error persists, try again later");
+                    }); // Fin del return
+                } // Fin del if
+                // PROCESADO DE RESPUESTA DESCONOCIDA
+                // Ha habido un error inesperado
+                else if (!response.ok) {
+                    return response.text().then(text => {
+                        throw new Error(text || "An unknown error happend");
+                    }); // Fin del return
+                } // Fin del if
+                return response;
+            })
+            // PROCESAR RESPUESTA OK
+                .then(data => {
+                    // Guardamos datos de en sesion
+                    //guardarDatosSeison(mail.value.trim());
+                    //msgBoxAccounts.style.color = "#5620ad";
+                    //msgBoxAccounts.textContent = "Account created successfuly.";
+                    //msgBoxAccounts.style.display = 'block';
+                    //window.location.href = 'movements.html';
+                    
+                    // RECORRER DATA Y GUARDARLO EN UN MAP PARA RETORNARLO
+//                    for (const account of data['account']) {
+//                        map.set(account['id'], 
+//                                new AccountController(account['id'],
+//                                            account['description'],
+//                                            account['balance'],
+//                                            account['creditLine'],
+//                                            account['beginBalance'],
+//                                            account['beginBalanceTimestamp'],
+//                                            account['type']));
+//                    }
+//                    return map;
+                    return data;
+            })
+            // MOSTRAR ERRORES
+                .catch(e => {
+//                    msgBoxAccounts.style.color = "#ff0000";
+//                    msgBoxAccounts.textContent = "Error: " + e.message;
+//                    msgBoxAccounts.style.display = 'block';
+            });
+    }
+    createAccount(account) {
+        // coger referencia de capa para mensaje 
+        // de informacion al usuario
+        const msgBoxAccounts = document.getElementById('msgBoxAccounts');
+        
+        fetch("/CRUDBankServerSide/webresources/account",
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(account)
+            }).then(response => {
+                // PROCESADO DE RESPUESTA 500
+                if (response.status===500) {
+                    return response.text().then(text => {
+                        throw new Error("An error happend, try again and if the error persists, try again later");
+                    }); // Fin del return
+                } // Fin del if
+                // PROCESADO DE RESPUESTA DESCONOCIDA
+                // Ha habido un error inesperado
+                else if (!response.ok) {
+                    return response.text().then(text => {
+                        throw new Error(text || "An unknown error happend");
+                    }); // Fin del return
+                } // Fin del if
+                return response;
+            })
+            // PROCESAR RESPUESTA OK
+                .then(data => {
+                    // Guardamos datos de en sesion
+                    //guardarDatosSeison(mail.value.trim());
+                    msgBoxAccounts.style.color = "#5620ad";
+                    msgBoxAccounts.textContent = "Account created successfuly.";
+                    msgBoxAccounts.style.display = 'block';
+                    //window.location.href = 'movements.html';
+                    const customerID= sessionStorage.getItem("curtomerID");
+                    if (customerID !== null) {
+                        return getAccountsByCustomerID(customerID);
+                    } else {
+                        throw new Error("No cusotmer ID storaged in the sesison");
+                    }
+            })
+            // MOSTRAR ERRORES
+                .catch(e => {
+                    msgBoxAccounts.style.color = "#ff0000";
+                    msgBoxAccounts.textContent = "Error: " + e.message;
+                    msgBoxAccounts.style.display = 'block';
+            });
+    }
+    updateAccount(updatedAccount) {
+        // Key: the updatedAccount should have 
+        // it's ID, to update it easly
+        
+    }
+    deleteAccount(accountID) {
+        
+    }
+}
